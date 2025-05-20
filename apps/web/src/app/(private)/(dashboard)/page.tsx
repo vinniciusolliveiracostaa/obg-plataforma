8515b1f564fab52a38eef0d57bf0da72dcf24a75
Vinicius Oliveira <vinnicius.olliveira.costaa@outlook.com.br>
2025-05-20 08:02:09 -0300

    feat: implementar CORS para permitir origens específicas e melhorar a segurança da API

--- apps/web/src/app/(private)/(dashboard)/page.tsx
+++ apps/web/src/app/(private)/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
-import {ChartAreaInteractive} from "@/components/chart-area-interactive";
-import {DataTable} from "@/components/data-table";
-import {SectionCards} from "@/components/section-cards";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
 
 import data from "./data.json";
 
